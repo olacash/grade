33c6017b01b75a771fe542a451a1d40c3e4440ca
olacash <40662416+olacash@users.noreply.github.com>
2018-07-04 13:53:36 +0100

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,19 @@
+var=int(input("put in a score"))
+if (var >=10  and  var <20):
+    print("you faill the course")
+elif(var >=30 and  var <40):
+    print("you score PASS")
+elif(var >= 40 and var <50):
+    print("your score is E'")
+elif(var >=50 and var <60):
+    print("your score is  D")
+elif(var >=60 and var <70):
+    print("your score is C")
+elif(var >=70 and var <80):
+          print("your score is  B")
+elif(var>=80 and var <90):
+    print ("your score is still B")
+elif(var>=100 and var <100):
+          print("your score is A")
+else:
+          print ("no comment is allowed")
